396b8bac643dd46721dbc007bc884cbe166be444
Richard Kirk <RichardBKirk@gmail.com>
2017-06-14 13:52:23 -0700

    xcode9 support

--- FontAwesome/FontAwesome.swift
+++ FontAwesome/FontAwesome.swift
@@ -149,7 +149,13 @@ private class FontLoader {
         let data = try! Data(contentsOf: fontURL)
 
         let provider = CGDataProvider(data: data as CFData)
-        let font = CGFont(provider!)
+        var font: CGFont
+        #if swift(>=3.2)
+            font = CGFont(provider!)!
+        #else
+            font = CGFont(provider!)
+        #endif
+        
 
         var error: Unmanaged<CFError>?
         if !CTFontManagerRegisterGraphicsFont(font, &error) {
