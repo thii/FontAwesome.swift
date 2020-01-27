c63b850cf88d18a0a19745441d1028cf393bb02e
Soheil Novinfard <novinfard@gmail.com>
2020-01-27 01:08:07 +0000

    Added gallery of icons to demo project

---
+++ Demo/FontAwesomeDemo/GalleryCell.swift
@@ -0,0 +1,13 @@
+//
+//  GalleryCell.swift
+//  FontAwesomeDemo
+//
+//  Created by Soheil Novinfard on 25/01/2020.
+//
+
+import UIKit
+
+class GalleryCell: UICollectionViewCell {
+	@IBOutlet weak var iconView: UIImageView!
+	@IBOutlet weak var titleLabel: UILabel!	
+}
