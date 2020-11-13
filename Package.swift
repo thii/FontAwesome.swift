481b6709deddd0893836000f9e7f50de11ac6148
MatrixSenpai <math.matrix@icloud.com>
2020-11-12 21:20:44 -0600

    Fixing issue where macOS would not recognize libswiftcore.dylib, updating fonts

--- Package.swift
+++ Package.swift
@@ -5,11 +5,16 @@ let package = Package(
     name: "FontAwesome",
     products: [
         .library(name: "FontAwesome", targets: ["FontAwesome"]),
+        .executable(name: "tools", targets: ["tools"])
     ],
     targets: [
         .target(
             name: "FontAwesome",
             path: "FontAwesome"
         ),
+        .target(
+            name: "tools",
+            path: "tools"
+        )
     ]
 )
